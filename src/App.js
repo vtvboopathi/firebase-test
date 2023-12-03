83e1480e3b5564bc11c1682e6dd878201a6ad0fa
Boopathiraja <vtvboopathi@gmail.com>
2023-12-04 00:39:43 +0530

    fire-base-test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from "react";
+import { db } from './firebase'
+import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [newName, setNewName] = useState('')
+  const [newAge, setNewAge] = useState(0)
+
+  const [users, setUsers] = useState([])
+  const usersCollection = collection(db, 'users')
+
+  const createUser = async () =>{
+    await addDoc(usersCollection, {name: newName, age: Number(newAge)})
+    setNewName('')
+    setNewAge('')
+  }
+
+  const updateUser = async (id, age) =>{
+    const userDoc = doc(db, 'users', id)
+    const newFields = {age: age + 1}
+    await updateDoc(userDoc, newFields)
+  }
+
+  const deleteuser = async (id) =>{
+    const userDoc = doc(db, 'users', id)
+    await deleteDoc(userDoc)
+  }
+
+  useEffect (()=>{
+    const getUsers = async () =>{
+      const data = await getDocs (usersCollection)
+      setUsers(data.docs.map((doc)=> ({...doc.data(), id:doc.id})))
+    }
+    getUsers()
+  }, [])
+
+  return(
+    <div className='hero'>
+      <input type='text' placeholder='Name...' onChange={(event)=> setNewName(event.target.value)}/>
+      <input type='number' placeholder='Age' onChange={(event)=> setNewAge(event.target.value)}/>
+      <button onClick={createUser}>Create User</button>
+      {users.map((user)=>{
+        return(
+          <div className='hero'>
+            <h1>{user.name}</h1>
+            <h1>{user.age}</h1>
+            <div className='btn-div'>
+            <button onClick={()=> {updateUser(user.id, user.age)}}>Update Age</button>
+            <button onClick={()=> {deleteuser(user.id)}}>Delete User</button>
+            </div>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
+       
 }
+
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFxSU6XfwwogwXpXoSWkfb2xlSO8XcomY",
+  authDomain: "fir-testing-940dd.firebaseapp.com",
+  projectId: "fir-testing-940dd",
+  storageBucket: "fir-testing-940dd.appspot.com",
+  messagingSenderId: "422643193689",
+  appId: "1:422643193689:web:fa1e363e48b5a626089e48",
+  measurementId: "G-0W9M8BKMNJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+
+
+
+
+
+// npm install -g firebase-tools
